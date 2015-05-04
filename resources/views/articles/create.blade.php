79dd1e33afd070107857f021096e7ad0155a4487
limit tsai <tsung.c.tsai@macrowell.com.tw>
2015-05-04 11:31:40 +0800

    create form

---
+++ resources/views/articles/create.blade.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Create Article</title>
+</head>
+<body>
+
+{!! Form::open(['route' => 'articles.index']) !!}
+<p>
+    Title: {!! Form::text('title') !!}
+</p>
+<p>
+    Body: {!! Form::textarea('body') !!}
+</p>
+<p>
+    {!! Form::submit('Create Article') !!}
+</p>
+{!! Form::close() !!}
+
+@if ($errors->any())
+<ul>
+    @foreach ($errors->all() as $error)
+    <li>{{ print_r($error) }}</li>
+    @endforeach
+</ul>
+@endif
+
+</body>
+</html>
